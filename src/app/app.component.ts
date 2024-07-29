aa5591d705e8dd1ccbf09a8d94e52bad9f9b8a79
Joanna Hulek <joanna.hulek@gmail.com>
2024-07-29 20:02:04 +0200

    typo fixed ;-)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'welcome in Angular Playground App!';
+  title = 'welcome to Angular Playground App!';
 }
