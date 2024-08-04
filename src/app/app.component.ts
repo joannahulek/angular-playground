c989d3500e2d25727c47b964bbd4b35f46bed922
Joanna Hulek <joanna.hulek@gmail.com>
2024-08-04 21:28:52 +0200

    code refactor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { WishItem } from "../shared/models/whishItem";
 import { RouterOutlet } from '@angular/router';
 import { NgForOf, NgIf } from "@angular/common";
-import { FormsModule } from '@angular/forms'; // Import FormsModule
+import { FormsModule } from '@angular/forms';
 
 const filters = [
   (item: WishItem) => item,
@@ -17,6 +17,7 @@ const filters = [
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'] // Corrected to styleUrls
 })
+
 export class AppComponent {
   items: WishItem[] = [
     new WishItem('To learn Angular'),
@@ -24,23 +25,25 @@ export class AppComponent {
     new WishItem('To dance bachata', false),
   ];
 
+  // visibility and filters
   listFilter: any = '0'
-
-  WishText = '';
 
   get visibleItems() : WishItem[] {
     return this.items.filter(filters[this.listFilter])
   }
+
+  toggleItem(item: WishItem) {
+    item.isComplete = !item.isComplete;
+    console.log(item);
+  }
+
+  // addWish() component
+  WishText = '';
 
   addWish() {
     if(this.WishText.trim()) {
       this.items.push(new WishItem(this.WishText));
       this.WishText = '';
     }
-  }
-
-  toggleItem(item: WishItem) {
-    item.isComplete = !item.isComplete;
-    console.log(item);
   }
 }
