6388e06d8ae15bf64c1fb1114bb24999c40cd3d0
Joanna Hulek <joanna.hulek@gmail.com>
2024-08-14 17:54:11 +0200

    old code removed

--- src/app/wish-list-item/wish-list-item.component.ts
+++ src/app/wish-list-item/wish-list-item.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {WishItem} from "../../shared/models/whishItem";
 import {NgClass} from "@angular/common";
 import {EventService} from "./../../shared/services/EventService"
