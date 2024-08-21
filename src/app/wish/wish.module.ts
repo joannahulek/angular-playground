e71a0c458c1a00dbc396bed7f7d0657611bccf96
Joanna Hulek <joanna.hulek@gmail.com>
2024-08-21 16:09:51 +0200

    Revert "moved to wish component"
    This reverts commit 1ca7faa29440fc607ea0085aee0f2df76d16b913.

--- src/app/wish/wish.module.ts
+++ src/app/wish/wish.module.ts
@@ -7,9 +7,6 @@ import { WishListComponent } from './wish-list/wish-list.component';
 import { AddWishFormComponent } from './add-wish-form/add-wish-form.component';
 import { WishFilterComponent } from './wish-filter/wish-filter.component';
 import { WishListItemComponent } from './wish-list-item/wish-list-item.component';
-import { WishComponent} from "./wish.component";
-
-import {WishService} from "./wish.service";
 
 @NgModule({
   declarations: [
@@ -24,8 +21,10 @@ import {WishService} from "./wish.service";
     HttpClientModule
   ],
   exports: [
-    WishComponent,
-    WishService
+    WishListComponent,
+    AddWishFormComponent,
+    WishFilterComponent,
+    WishListItemComponent
   ]
 })
 export class WishModule { }
