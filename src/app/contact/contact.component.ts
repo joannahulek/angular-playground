210e8ea13c2eb1217b83a791f74f23612d340673
Joanna Hulek <joanna.hulek@gmail.com>
2024-08-24 16:47:24 +0200

    Validators handling added, custom validator (domain) created

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,11 +1,17 @@
 import {Component, OnInit} from '@angular/core';
 import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {NgIf} from "@angular/common";
+
+import {domainValidator} from "./invalidEmailDomain";
+
+const invalidEmailDomain = domainValidator(['a.bc', 'ab.cd'])
 
 @Component({
   selector: 'app-contact',
   standalone: true,
   imports: [
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    NgIf
   ],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
@@ -16,8 +22,8 @@ export class ContactComponent implements OnInit{
 
   contactForm = new FormGroup({
     senderName: new FormControl('', Validators.required),
-    senderEmail: new FormControl('', Validators.required),
-    senderMessage: new FormControl('', Validators.required)
+    senderEmail: new FormControl('', [Validators.required, Validators.email, invalidEmailDomain]),
+    senderMessage: new FormControl('', [Validators.required, Validators.minLength(10), Validators.maxLength(10000)])
   })
 
   submitForm(){
